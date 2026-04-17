e276221b466734dcb3bc3c51f0a84a558494810e
325103210038kusalabai-source <325103210038.kusalabai@gvpcew.ac.in>
2026-04-17 17:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector(".hero button");
-
-    button.addEventListener("click", function() {
-        alert("Welcome to Tasty Bites! Explore our delicious menu 🍽️");
+document.querySelector(".hero button").addEventListener("click", function() {
+    document.querySelector(".menu").scrollIntoView({
+        behavior: "smooth"
     });
 });
